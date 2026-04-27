f58e55e262c1d8a64844f36c1e6582be1b6268b5
Thiago Borges <tblima26@gmail.com>
2026-04-27 15:26:37 -0300

    Projeto YOU CAST finalizado.

---
+++ atividades/atividade-you-cast/js/script.js
@@ -0,0 +1,49 @@
+
+
+const feed = document.querySelector('.feed');
+console.log(feed);
+
+const nicks = [
+  "ghosty", "vibez", "darkman", "lunarx", "codezera",
+  "chillboy", "astrozin", "ninj4", "pixelado", "fluxo"
+];
+
+const tags = [
+  "#liveNow", "#gaming", "#music", "#randomTalks",
+  "#coding", "#fun", "#streamLife"
+];
+
+function randomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+/* cria array de usuários */
+const users = Array.from({ length: 12 }, () => ({
+  nick: randomItem(nicks) + Math.floor(Math.random() * 100),
+  tag: randomItem(tags)
+}));
+
+/* mantém seu padrão */
+users.forEach((user, i) => {
+  const views = Math.floor(Math.random() * 1000);
+
+  const card = `
+    <div class="card">
+      <img class="cover" src="img/live/online-${i+1}.jpg" alt="live">
+
+      <div class="card-info">
+        <img class="avatar" src="img/profile/p${(i%6)+1}.jpg">
+
+        <div class="user">
+          <div class="nick">${user.nick}</div>
+          <div class="tag">${user.tag}</div>
+        </div>
+
+        <div class="views">
+          <i class="fa-solid fa-eye"></i> ${views}
+        </div>
+      </div>
+    </div>
+  `;
+  feed.innerHTML += card;
+});
