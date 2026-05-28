ae73e17d7740e14b8d4d1e7579e25ba13cdf8587
tblima26 <tblima26@gmail.com>
2026-05-28 20:59:13 -0300

    Java Lanches - Novo Produto - Data: 28/05/2026

--- parte06-spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
+++ parte06-spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
@@ -2,6 +2,7 @@ package com.crud.javalanches.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -27,5 +28,10 @@ public class JavalanchesController {
     public String novaCategoria(Categoria categoria) {
         categoriaRepository.save(categoria);
         return "categoria_sucesso";
+    }
+    @GetMapping("/novoProduto")
+    public String novoProduto(Model model) {
+        model.addAttribute("categorias", categoriaRepository.findAll());
+        return "novo_produto";
     }
 }
