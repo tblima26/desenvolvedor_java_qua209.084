0c3baf5932cd0e58119f290603325094c2bde743
tblima26 <tblima26@gmail.com>
2026-05-28 21:43:22 -0300

    Java Lanches - Novo Produto - Data: 28/05/2026

--- parte06-spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
+++ parte06-spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crud.javalanches.models.Categoria;
+import com.crud.javalanches.models.Produto;
 import com.crud.javalanches.repository.CategoriaRepository;
+import com.crud.javalanches.repository.ProdutoRepository;
 
 @Controller
 public class JavalanchesController {
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     @GetMapping("/")
     public String index() {
@@ -29,9 +34,18 @@ public class JavalanchesController {
         categoriaRepository.save(categoria);
         return "categoria_sucesso";
     }
+
     @GetMapping("/novoProduto")
     public String novoProduto(Model model) {
         model.addAttribute("categorias", categoriaRepository.findAll());
         return "novo_produto";
+    }
+
+    @PostMapping("/novoProduto")
+    public String novoProduto(Produto produto, @RequestParam("categoriaId") Long categoriaId) {
+        Categoria categoria = categoriaRepository.findById(categoriaId).orElse(null);
+        produto.setCategoria(categoria);
+        produtoRepository.save(produto);
+        return "produto_sucesso";
     }
 }
