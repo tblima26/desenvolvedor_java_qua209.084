944a7b7e56b48ddae43ea4a21c48293ec6cae3b2
FernandoT <fernandotmgomes@gmail.com>
2026-06-11 21:36:13 -0300

    Java Lanches - Listar clientes - Data: 11/06/2026

--- parte06-spring/04-javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
+++ parte06-spring/04-javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
@@ -107,5 +107,20 @@ public class JavalanchesController {
         return "atualizar_categoria_sucesso";
     }
 
-    
+    @GetMapping("/atualizarProduto")
+    public String atualizarProduto(@RequestParam("codigoProduto") Long codigoProduto, Model model) {
+        Produto produto = produtoRepository.findById(codigoProduto).orElse(null);
+        model.addAttribute("produto", produto);
+        model.addAttribute("categorias", categoriaRepository.findAll());
+        return "atualizar_produto";
+    }
+
+    @PostMapping("/atualizarProduto")
+    public String atualizarProduto(@RequestParam("categoriaId") Long categoriaId, Produto produto) {
+        Categoria categoria = categoriaRepository.findById(categoriaId).orElse(null);
+        produto.setCategoria(categoria);
+        produtoRepository.save(produto);
+        return "atualizar_produto_sucesso";
+    }
+
 }
